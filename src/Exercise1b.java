82d855b4d2208a81453023c4af09ff7f354167c0
mrtrunghieu1 <trunghieu02111998@gmail.com>
2023-06-20 15:10:19 +0700

    Exercise Java Book

---
+++ src/Exercise1b.java
@@ -0,0 +1,25 @@
+// Sample A
+class Exercise1b {
+	public static void main(String[] args) {
+		int x = 1;
+		while (x < 10) {
+			if (x > 3) {
+				System.out.println("big x");
+			}
+			x = x + 1;
+		}
+	}
+}
+
+// Sample B
+//class Exercise1b {
+//	public static void main(String[] args) {
+//		int x = 5;
+//		while (x > 1) {
+//			x = x - 1;
+//			if (x < 3) {
+//				System.out.println("small x");
+//			}
+//		}
+//	}
+//}
